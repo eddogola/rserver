63d7049f22ccede41b36a79bb1c6f221920fd700
Eddy Ogola <eddy.ogola1@gmail.com>
2022-06-29 05:57:55 -0700

    Put the receiving end of channel in smart pointer
    an Arc and Mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::thread;
+use std::sync::mpsc; // fifo queue providing message-based communication over channels
+use std::sync::Arc;
+use std::sync::Mutex;
 
 struct Worker {
     id: usize,
@@ -6,17 +9,22 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(|| {})
+            thread: thread::spawn(|| {
+                receiver;
+            })
         }
     }
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
+
+struct Job;
 
 impl ThreadPool {
     
@@ -25,12 +33,18 @@ impl ThreadPool {
 
         let mut workers = Vec::with_capacity(size); // empty vector of length size
 
+        // create a channel
+        let (sender, receiver) = mpsc::channel();
+        // put the receiving end in an Arc and a Mutex
+        let receiver = Arc::new(Mutex::new(receiver));
+
         for i in 0..size {
             // create workers and store in workers vector
-            workers.push(Worker::new(i));
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        // move channel sender to the threadpool
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
